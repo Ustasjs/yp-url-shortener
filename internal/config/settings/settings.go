940a8f16b9d471784de494f3eb97634d7b1034f4
Stanislav Ustiuzhanin <su@mobalyticshq.com>
2026-02-13 11:09:13 +0700

    fix: add envs

--- internal/config/settings/settings.go
+++ internal/config/settings/settings.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"flag"
 	"net/url"
+	"os"
 	"strings"
 )
 
@@ -28,39 +29,70 @@ func InitSettings() *Settings {
 
 var errorMessageServerAddress = "invalid server address. Expected format: host:port"
 
+func validateServerAddress(serverAddress string) error {
+	value := strings.Split(serverAddress, ":")
+	if len(value) != 2 {
+		return errors.New(errorMessageServerAddress)
+	}
+	host := value[0]
+	port := value[1]
+	if host == "" || port == "" {
+		return errors.New(errorMessageServerAddress)
+	}
+	return nil
+}
+
 func initServerAddress(settings *Settings) {
 	var serverAddressValue ServerAddress = "localhost:8080"
 	settings.ServerAddress = serverAddressValue
 
 	flag.Func("a", "Input server address", func(flagValue string) error {
-		value := strings.Split(flagValue, ":")
-		if len(value) != 2 {
-			return errors.New(errorMessageServerAddress)
-		}
-		host := value[0]
-		port := value[1]
-		if host == "" || port == "" {
-			return errors.New(errorMessageServerAddress)
+		err := validateServerAddress(flagValue)
+		if err != nil {
+			return err
 		}
 		settings.ServerAddress = ServerAddress(flagValue)
 		return nil
 	})
+
+	if envServerAddress := os.Getenv("SERVER_ADDRESS"); envServerAddress != "" {
+		err := validateServerAddress(envServerAddress)
+		if err != nil {
+			panic(err)
+		}
+		settings.ServerAddress = ServerAddress(envServerAddress)
+	}
 }
 
 var errorMessageBaseURL = "invalid base url. Expected format: http://host:port"
+
+func validateBaseURL(baseURL string) error {
+	parsedURL, err := url.ParseRequestURI(baseURL)
+	if err != nil || parsedURL.Scheme == "" || parsedURL.Host == "" {
+		return errors.New(errorMessageBaseURL)
+	}
+	return nil
+}
 
 func initBaseURL(settings *Settings) {
 	var baseURLvalue BaseURL = "http://localhost:8080"
 	settings.BaseURL = baseURLvalue
 
 	flag.Func("b", "Input base url", func(flagValue string) error {
-		parsedURL, err := url.ParseRequestURI(flagValue)
-
-		if err != nil || parsedURL.Scheme == "" || parsedURL.Host == "" {
-			return errors.New(errorMessageBaseURL)
+		err := validateBaseURL(flagValue)
+		if err != nil {
+			return err
 		}
 
 		settings.BaseURL = BaseURL(flagValue)
 		return nil
 	})
+
+	if envBaseURL := os.Getenv("BASE_URL"); envBaseURL != "" {
+		err := validateBaseURL(envBaseURL)
+		if err != nil {
+			panic(err)
+		}
+		settings.BaseURL = BaseURL(envBaseURL)
+	}
 }
