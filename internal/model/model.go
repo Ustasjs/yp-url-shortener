5d087b02f80df551c7f48032e2a8a4141b191d0d
Stanislav Ustiuzhanin <su@mobalyticshq.com>
2026-03-11 11:52:35 +0700

    feat: implement batch saving

--- internal/model/model.go
+++ internal/model/model.go
@@ -7,3 +7,13 @@ type CreateShortURLRequest struct {
 type CreateShortURLResponce struct {
 	Result string `json:"result"`
 }
+
+type BatchShortURLRequestItem struct {
+	CorrelationID string `json:"correlation_id"`
+	OriginalURL   string `json:"original_url"`
+}
+
+type BatchShortURLResponseItem struct {
+	CorrelationID string `json:"correlation_id"`
+	ShortURL      string `json:"short_url"`
+}
