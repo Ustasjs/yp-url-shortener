a505b3c10b07d0e5e5d0ac2f99e4b10dd2047f05
Stanislav Ustiuzhanin <su@mobalyticshq.com>
2026-03-11 12:22:48 +0700

    wip

--- internal/repository/postgres.go
+++ internal/repository/postgres.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"Ustasjs/yp-url-shortener/internal/model"
 	"context"
 	"database/sql"
 	"errors"
@@ -33,4 +34,29 @@ func (s *PostgresStorage) Get(ctx context.Context, id string) (string, error) {
 		return "", ErrRecordNotFound
 	}
 	return originalURL, err
+}
+
+func (s *PostgresStorage) SaveListUrls(ctx context.Context, records []model.ShortURLRecord) error {
+	if len(records) == 0 {
+		return nil
+	}
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	stmt, err := tx.PrepareContext(ctx, `INSERT INTO short_urls (short_id, original_url) VALUES ($1, $2) ON CONFLICT (original_url) DO NOTHING`)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	for _, rec := range records {
+		_, err = stmt.ExecContext(ctx, rec.ID, rec.URL)
+		if err != nil {
+			return err
+		}
+	}
+	return tx.Commit()
 }
