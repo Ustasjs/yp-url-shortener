57699c38365d71eaafb36b255928e3dd31372d3f
Stanislav Ustiuzhanin <su@mobalyticshq.com>
2026-06-19 13:14:50 +0700

    logger

--- internal/logger/responce_writer.go
+++ internal/logger/responce_writer.go
@@ -13,12 +13,16 @@ type (
 	}
 )
 
+// Write writes b to the underlying ResponseWriter and accumulates the response
+// size.
 func (r *loggingResponseWriter) Write(b []byte) (int, error) {
 	size, err := r.ResponseWriter.Write(b)
 	r.responseData.size += size
 	return size, err
 }
 
+// WriteHeader writes statusCode to the underlying ResponseWriter and records it
+// for logging.
 func (r *loggingResponseWriter) WriteHeader(statusCode int) {
 	r.ResponseWriter.WriteHeader(statusCode)
 	r.responseData.status = statusCode
